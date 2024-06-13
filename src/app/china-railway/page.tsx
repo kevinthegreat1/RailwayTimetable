8702b54089cf575bbd7f9ec13b2d875f50b8a88a
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-06-13 16:58:11 +0800

    Add routes selection

--- src/app/china-railway/page.tsx
+++ src/app/china-railway/page.tsx
@@ -1,9 +1,26 @@
 import RouteEntry from "@/components/route-entry";
 
-export default function Timetable() {
+export default async function Timetable() {
+  const stationsString = await fetch("https://kyfw.12306.cn/otn/resources/js/framework/station_name.js"); // TODO useEffect or useData or static route handler (probably route handler)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-sky-100">
-      <RouteEntry/>
+    <main className="h-dvh bg-sky-50">
+      <div className="flex flex-col items-stretch justify-between p-4">
+        <div className="m-4 p-4 rounded-3xl bg-sky-100">
+          <RouteEntry date={true}/>
+        </div>
+        <ul className="m-4 px-4 divide-y rounded-3xl bg-sky-100 divide-blue-200">
+          <li className="py-4 flex items-center">
+            <div className="grow basis-0"></div>
+            <div className="text-xl">路径</div>
+            <div className="grow basis-0 flex justify-end">
+              <button className="w-8 h-8 rounded-full text-lg bg-sky-200">⊕</button>
+            </div>
+          </li>
+          <li className="py-2"><RouteEntry bothWayOption={true}/></li>
+          <li className="py-2"><RouteEntry bothWayOption={true}/></li>
+        </ul>
+      </div>
     </main>
   )
 }
--- src/components/route-entry.tsx
+++ src/components/route-entry.tsx
@@ -1,18 +1,29 @@
-export default function RouteEntry() {
+export type RouteEntryProps = {
+  fromStationText?: string,
+  toStationText?: string,
+  date?: boolean
+  bothWayOption?: boolean
+};
+
+export default function RouteEntry({fromStationText = "出发地", toStationText = "目的地", date = false, bothWayOption = false}: RouteEntryProps) {
   return (
-    <div className="flex min-w-full justify-around">
+    <div className="flex justify-around">
       <div className="flex flex-col items-center">
-        <label htmlFor="fromStation">出发地</label>
-        <input id="fromStation"></input>
+        <label htmlFor="fromStation">{fromStationText}</label>
+        <input id="fromStation" className="px-1 rounded-lg"></input>
       </div>
       <div className="flex flex-col items-center">
-        <label htmlFor="toStation">目的地</label>
-        <input id="toStation"></input>
+        <label htmlFor="toStation">{toStationText}</label>
+        <input id="toStation" className="px-1 rounded-lg"></input>
       </div>
-      <div className="flex flex-col items-center">
+      {date && <div className="flex flex-col items-center">
         <label htmlFor="date">日期</label>
-        <input id="date" placeholder={new Date().toISOString().split('T')[0]}></input>
-      </div>
+        <input id="date" className="px-1 rounded-lg" placeholder={new Date().toISOString().split('T')[0]}></input>
+      </div>}
+      {bothWayOption && <div className="flex flex-col items-center">
+        <label htmlFor="bothWay">双向</label>
+        <input id="bothWay" type="checkbox" checked={true} className="m-2"/>
+      </div>}
     </div>
   )
 }
